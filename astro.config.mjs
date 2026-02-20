9830f6253fa408a78f6ca719f7b8d980794b111b
Fadi <fadikirbag5@gmail.com>
2026-02-19 21:30:01 -0400

    modified astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import path from 'path';
 
 export default defineConfig({
+
+    site: 'https://fadikirbag.pages.dev',
+    
+    output: 'static',
+
     vite: {
         resolve: {
-        alias: {
-            '@': path.resolve('./src'),
-        },
+            alias: {
+                '@': path.resolve('./src'),
+            },
         },
     },
     devToolbar: {
